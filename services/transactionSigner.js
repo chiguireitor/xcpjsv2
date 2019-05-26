eb8bbe511c7b6bba2431b3a9ad5200151f59ffc3
Ben Smith <shibuyashadows@gmail.com>
2019-05-26 12:32:48 +0900

    adds support for p2shp2ws

--- services/transactionSigner.js
+++ services/transactionSigner.js
@@ -51,5 +51,28 @@ module.exports = {
     }
 
     return address
+  },
+
+  wifKeyP2SHP2WSHSigner: (pubkeys, wif, m, networkName) => {
+    const network = bitcoin.networks[networkName]
+    const keyPair = bitcoin.ECPair.fromWIF(wif, network)
+    const pkeys = pubkeys.map((hex) => Buffer.from(hex, 'hex'))
+    const p2ms = bitcoin.payments.p2ms({ m: m, pubkeys, network: network })
+    const p2wsh = bitcoin.payments.p2wsh({ redeem: p2ms, network: network })
+    const p2sh = bitcoin.payments.p2sh({ redeem: p2wsh, network: network })
+    const address = p2sh.address
+    registeredSigners[keyPair.publicKey.toString("hex")] = async (tx) => {
+      for (let i=0; i < tx.__inputs.length; i++) {
+        try {
+          tx.sign(i, keyPair,p2sh.redeem.output)
+        } catch(e) {
+          console.log(e)
+        }
+      }
+      return tx
+    }
+
+    return address
   }
+  
 }
