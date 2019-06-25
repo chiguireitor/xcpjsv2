c4192a78934e2863ec0e52dfaf348603f219b2c1
John Villar <johnvillar@contimita.com>
2019-06-25 16:18:11 -0400

    Added stochastic pick to utxo selection

--- services/indexdUtxos.js
+++ services/indexdUtxos.js
@@ -1,27 +1,54 @@
 const axios = require('axios')
+const crypto = require('crypto')
 
 const BYTES_PER_USED_UTXO = 40
 
 function pickUtxosOldestFirst(utxos, len, feePerByte, additionalNeededValue) {
+  let ordered = utxos.sort((a, b) => b.height - a.height)
+
+  return pickFromSet(ordered, len, feePerByte, additionalNeededValue)
+}
+
+async function pickRandomized(utxos, len, feePerByte, additionalNeededValue) {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(4, (err, buf) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      let p = buf.readUInt32LE()
+      let rnd = () => {
+        let v = p & 3
+        p = (p >>> 2) | (v <<< 30)
+        return v - 1
+      }
+      let ordered = utxos.sort((a, b) => rnd())
+
+      return pickFromSet(ordered, len, feePerByte, additionalNeededValue)
+    })
+  })
+}
+
+function pickFromSet(ordered, len, feePerByte, additionalNeededValue) {
   let feeSats = Math.round(len * feePerByte) + additionalNeededValue
 
-  let ordered = utxos.sort((a, b) => b.height - a.height)
+  let value = 0
   let picked = []
 
-  while (ordered.length > 0 && feeSats > 0) {
+  while (ordered.length > 0 && feeSats > value) {
     let utxo = ordered.pop()
 
-    //if (utxo.value > feePerByte * BYTES_PER_USED_UTXO) {
     if (utxo.value > feePerByte * BYTES_PER_USED_UTXO && utxo.value > 5430 && ((utxo.coinbase == 0) || (utxo.coinbase == 1 && utxo.confirmations > 100))) {
       picked.push(utxo)
-      feeSats -= utxo.value + feePerByte * BYTES_PER_USED_UTXO
+      value += utxo.value
+      feeSats += feePerByte * BYTES_PER_USED_UTXO
     }
   }
 
-  if (feeSats <= 0) {
+  if (value - feeSats >= 0) {
     return {
       remainingUtxos: ordered,
-      picked, change: -feeSats
+      picked, change: value - feeSats
     }
   } else {
     throw new Error('Not enough balance, reduce fee')
@@ -57,14 +84,25 @@ module.exports = baseURL => {
               throw new Error('UTXO not found')
             }
           } else if ('approximateByteLength' in ops && 'targetFeePerByte' in srcOps) {
-            let {
-              remainingUtxos,
-              picked,
-              change
-            } = pickUtxosOldestFirst(cachedUtxos, ops.approximateByteLength, srcOps.targetFeePerByte, ops.additionalNeededValue)
+            if (srcOps.stochasticPick) {
+              let {
+                remainingUtxos,
+                picked,
+                change
+              } = (await pickRandomized(cachedUtxos, ops.approximateByteLength, srcOps.targetFeePerByte, ops.additionalNeededValue))
 
-            cachedUtxos = remainingUtxos
-            return { utxos: picked, change }
+              cachedUtxos = remainingUtxos
+              return { utxos: picked, change }
+            } else {
+              let {
+                remainingUtxos,
+                picked,
+                change
+              } = pickUtxosOldestFirst(cachedUtxos, ops.approximateByteLength, srcOps.targetFeePerByte, ops.additionalNeededValue)
+
+              cachedUtxos = remainingUtxos
+              return { utxos: picked, change }
+            }
           } else {
             return [cachedUtxos.shift()]
           }
