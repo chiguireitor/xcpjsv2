5470c0d6ff00bf15632848b3a7f5b0d4c2426b15
ben <ben@pop-os.localdomain>
2019-12-21 14:50:55 +0900

    adds initialize export

--- index.js
+++ index.js
@@ -1,72 +1,74 @@
-(function()
-{
+class xcpjsv2 {
 
-  var exports = module.exports = {};
+  constructor() {
 
-  const bitcoin = require('bitcoinjs-lib')
-  const messages = require('./messages')
-  const envelopes = require('./envelopes')
-  const services = require('./services')
+    this.bitcoin = require('bitcoinjs-lib')
+    this.messages = require('./messages')
+    this.envelopes = require('./envelopes')
+    this.services = require('./services')
+    this.network = {
+      messagePrefix: '\u0018Bitcoin Signed Message:\n',
+      bech32: 'tb',
+      bip32: {
+        public: 70617039,
+        private: 70615956
+      },
+      pubKeyHash: 100,
+      scriptHash: 40,
+      wif: 239
+    }
+    this.utxoService = null
+    this.broadcastService = null
 
-  let network = {
-    messagePrefix: '\u0018Bitcoin Signed Message:\n',
-    bech32: 'tb',
-    bip32: {
-      public: 70617039,
-      private: 70615956
-    },
-    pubKeyHash: 100,
-    scriptHash: 40,
-    wif: 239
   }
 
-  let utxoService, broadcastService
 
-  exports.sendraw = async function(source, destination, asset, quantity, memo, memoIsHex, coinSelect) {
-    let msg = messages.send.compose(asset, destination, quantity, memo, memoIsHex)
-    return _envelopeAndBuild_(source, msg, true, coinSelect)
+
+  async function sendraw(source, destination, asset, quantity, memo, memoIsHex, coinSelect) {
+    let msg = this.messages.send.compose(asset, destination, quantity, memo, memoIsHex)
+    return this._envelopeAndBuild_(source, msg, true, coinSelect)
   }
 
-  exports.send = async function(source, destination, asset, quantity, memo, memoIsHex) {
-    let msg = messages.send.compose(asset, destination, quantity, memo, memoIsHex)
-    return _envelopeAndBuild_(source, msg, false)
+  async function send(source, destination, asset, quantity, memo, memoIsHex) {
+    let msg = this.messages.send.compose(asset, destination, quantity, memo, memoIsHex)
+    return this._envelopeAndBuild_(source, msg, false)
   }
 
-  exports.order = async function (source, giveAsset, giveQuantity, getAsset, getQuantity) {
-    let msg = messages.order.compose(source, giveAsset, giveQuantity, getAsset, getQuantity)
-    return _envelopeAndBuild_(source, msg, false)
+  async function order(source, giveAsset, giveQuantity, getAsset, getQuantity) {
+    let msg = this.messages.order.compose(source, giveAsset, giveQuantity, getAsset, getQuantity)
+    return this._envelopeAndBuild_(source, msg, false)
   }
 
-  exports.issuanceraw = async function (source, transferDestination, asset, quantity, divisible, description, coinSelect) {
+  async function issuanceraw(source, transferDestination, asset, quantity, divisible, description, coinSelect) {
     console.log("this is issuanceraw coinSelect", coinSelect)
-    let msg = messages.issuance.compose(source, transferDestination, asset, quantity, divisible, description)
+    let msg = this.messages.issuance.compose(source, transferDestination, asset, quantity, divisible, description)
     console.log("this is issuanceraw coinSelect", coinSelect)
-    return _envelopeAndBuild_(source, msg, true, coinSelect)
+    return this._envelopeAndBuild_(source, msg, true, coinSelect)
   }
 
-  exports.issuance = async function (source, transferDestination, asset, quantity, divisible, description) {
+  async function issuance(source, transferDestination, asset, quantity, divisible, description) {
     let msg = messages.issuance.compose(source, transferDestination, asset, quantity, divisible, description)
     return _envelopeAndBuild_(source, msg, false)
   }
 
-  exports.broadcast = async function (source, timestamp, value, feeFraction, text) {
-    let msg = messages.broadcast.compose(source, timestamp, value, feeFraction, text)
-    return _envelopeAndBuild_(source, msg, false)
+  async function broadcast(source, timestamp, value, feeFraction, text) {
+    let msg = this.messages.broadcast.compose(source, timestamp, value, feeFraction, text)
+    return this._envelopeAndBuild_(source, msg, false)
   }
 
-  exports.broadcastRawTx = async function (tx) {
+  async function broadcastRawTx(tx) {
     let broadcastResult = await broadcastService.broadcast(tx)
     return broadcastResult
   }
 
-  exports._envelopeAndBuild_ = async function (source, msg, getraw, cs) {
+  async function _envelopeAndBuild_(source, msg, getraw, cs) {
     let additionalOutputs = null
     if (typeof(msg) === 'object' && !Buffer.isBuffer(msg)) {
       additionalOutputs = msg.outputs
       msg = msg.msgData
     }
 
-    let addrUtxoService = utxoService.forAddress(source, {
+    let addrUtxoService = this.utxoService.forAddress(source, {
       targetFeePerByte: 10
     })
 
@@ -74,30 +76,30 @@
 
     console.log("this is _envelopeAndBuild_ cs", coinSelect)
     console.log("pre opreturn params", msg, addrUtxoService, additionalOutputs, coinSelect)
-    let envelope = await envelopes.opreturn(msg, addrUtxoService, additionalOutputs, coinSelect)
+    let envelope = await this.envelopes.opreturn(msg, addrUtxoService, additionalOutputs, coinSelect)
     envelope.inputs = envelope.coinSelect.utxos
-    let unsignedTxBuilder = await services.transactionBuilder(network, envelope, additionalOutputs)
-    await services.transactionSigner.sign(source, unsignedTxBuilder)
+    let unsignedTxBuilder = await this.services.transactionBuilder(network, envelope, additionalOutputs)
+    await this.services.transactionSigner.sign(source, unsignedTxBuilder)
 
     if (getraw) {
       return [unsignedTxBuilder.buildIncomplete().toHex(), envelope.coinSelect]
     }
 
     let txHex = unsignedTxBuilder.build().toHex()
-    let broadcastResult = await broadcastService.broadcast(txHex)
+    let broadcastResult = await this.broadcastService.broadcast(txHex)
     return broadcastResult
   }
 
-  exports.setNetwork = function (name) {
-    network = bitcoin.networks[name]
+  function setNetwork(name) {
+    this.network = this.bitcoin.networks[name]
   }
 
-  exports.setUtxoService = function (srv) {
-    utxoService = srv
+  function setUtxoService(srv) {
+    this.utxoService = srv
   }
 
-  exports.setBroadcastService = function (srv) {
-    broadcastService = srv
+  function setBroadcastService(srv) {
+    this.broadcastService = srv
   }
 
   // module.exports = {
@@ -114,6 +116,4 @@
   //   broadcastRawTx
   // }
 
-  return exports;
-
-})();
+}
