575fa6e87f67777097067d2afbae1acded998e32
ben <ben@pop-os.localdomain>
2019-12-21 15:04:03 +0900

    adds initialize export

--- index.js
+++ index.js
@@ -24,44 +24,44 @@ class XCPJS {
 
 
 
-  async function sendraw(source, destination, asset, quantity, memo, memoIsHex, coinSelect) {
+  async sendraw(source, destination, asset, quantity, memo, memoIsHex, coinSelect) {
     let msg = this.messages.send.compose(asset, destination, quantity, memo, memoIsHex)
     return this._envelopeAndBuild_(source, msg, true, coinSelect)
   }
 
-  async function send(source, destination, asset, quantity, memo, memoIsHex) {
+  async send(source, destination, asset, quantity, memo, memoIsHex) {
     let msg = this.messages.send.compose(asset, destination, quantity, memo, memoIsHex)
     return this._envelopeAndBuild_(source, msg, false)
   }
 
-  async function order(source, giveAsset, giveQuantity, getAsset, getQuantity) {
+  async order(source, giveAsset, giveQuantity, getAsset, getQuantity) {
     let msg = this.messages.order.compose(source, giveAsset, giveQuantity, getAsset, getQuantity)
     return this._envelopeAndBuild_(source, msg, false)
   }
 
-  async function issuanceraw(source, transferDestination, asset, quantity, divisible, description, coinSelect) {
+  async issuanceraw(source, transferDestination, asset, quantity, divisible, description, coinSelect) {
     console.log("this is issuanceraw coinSelect", coinSelect)
     let msg = this.messages.issuance.compose(source, transferDestination, asset, quantity, divisible, description)
     console.log("this is issuanceraw coinSelect", coinSelect)
     return this._envelopeAndBuild_(source, msg, true, coinSelect)
   }
 
-  async function issuance(source, transferDestination, asset, quantity, divisible, description) {
+  async issuance(source, transferDestination, asset, quantity, divisible, description) {
     let msg = messages.issuance.compose(source, transferDestination, asset, quantity, divisible, description)
     return _envelopeAndBuild_(source, msg, false)
   }
 
-  async function broadcast(source, timestamp, value, feeFraction, text) {
+  async broadcast(source, timestamp, value, feeFraction, text) {
     let msg = this.messages.broadcast.compose(source, timestamp, value, feeFraction, text)
     return this._envelopeAndBuild_(source, msg, false)
   }
 
-  async function broadcastRawTx(tx) {
+  async broadcastRawTx(tx) {
     let broadcastResult = await broadcastService.broadcast(tx)
     return broadcastResult
   }
 
-  async function _envelopeAndBuild_(source, msg, getraw, cs) {
+  async _envelopeAndBuild_(source, msg, getraw, cs) {
     let additionalOutputs = null
     if (typeof(msg) === 'object' && !Buffer.isBuffer(msg)) {
       additionalOutputs = msg.outputs
@@ -90,15 +90,15 @@ class XCPJS {
     return broadcastResult
   }
 
-  function setNetwork(name) {
+   setNetwork(name) {
     this.network = this.bitcoin.networks[name]
   }
 
-  function setUtxoService(srv) {
+   setUtxoService(srv) {
     this.utxoService = srv
   }
 
-  function setBroadcastService(srv) {
+   setBroadcastService(srv) {
     this.broadcastService = srv
   }
 
