7df8a111759d49b0245763d18cf276621af4a35d
ben <ben@pop-os.localdomain>
2020-05-07 12:30:46 +0900

    adds native tx support

--- envelopes/index.js
+++ envelopes/index.js
@@ -4,11 +4,8 @@ const addPrefix = (fn) => (data, utxoService, additionalOutputs,coinSelect,netwo
   return fn(Buffer.concat([PREFIX, data]), utxoService, additionalOutputs,coinSelect,network)
 }
 
-const wrapFunction = (fn) => (addrUtxoService, amount, address,coinSelect, network) => {
-  return fn(addrUtxoService, amount, address,coinSelect, network)
-}
 
 module.exports = {
   opreturn: addPrefix(require('./opreturn')),
-  native: wrapFunction(require('./native'))
+  native: require('./native')()
 }
